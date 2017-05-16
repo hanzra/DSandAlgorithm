b7513e1fa5a879eacca16a1f0b22148c850d6e07
shanzra <shanzra@commvault.com>
2017-05-16 16:46:18 -0400

    BinarySearchFinish

---
+++ DataStructures/BinarySearch/BinarySearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearch
+{    class BinarySearch
+    {
+
+        int[] arr = new int[] {2,4,5,7,10,33,44,55,90,100};
+
+        
+        public bool Search(int number)
+        {
+            int lo = 0;
+            int hi = 9;            
+            int comparison = 1;
+
+
+            while (lo <= hi)
+            {
+                int Mid = lo + (hi - lo) / 2;
+                if (arr[Mid] == number)
+                {
+                    OutPut(comparison, true);
+                    return true;
+                }
+                else if (arr[Mid] < number)
+                {
+                    lo = Mid + 1;
+                }
+                else
+                {
+                    hi = Mid - 1;
+                }
+                comparison++;
+            }
+            OutPut(comparison, false);
+            return false;
+        }
+
+        public void OutPut(int compariosns, bool found)
+        {
+            Console.WriteLine($"Found: {found} Comparisons: {compariosns}");
+        }
+
+
+
+    }
+}
--- DataStructures/BinarySearch/Program.cs
+++ DataStructures/BinarySearch/Program.cs
@@ -6,7 +6,17 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinarySearch br = new BinarySearch();
+            int searchnumber;
+            string moreSearch = "Y";
+            do
+            {
+                Console.WriteLine("enternumber");
+                searchnumber = Convert.ToInt32(Console.ReadLine());
+                br.Search(searchnumber);
+                Console.WriteLine("More Search?");
+                moreSearch = Console.ReadLine().ToUpper();                
+            } while (moreSearch=="Y");
         }
     }
 }
